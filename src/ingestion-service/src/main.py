178f5a18c588ccdeff138e12845dd2cd92679a76
nsgorlew <nsgorlew@gmail.com>
2023-10-15 09:07:47 -0400

    added persistence

--- src/ingestion-service/src/main.py
+++ src/ingestion-service/src/main.py
@@ -1,45 +1,45 @@
-from fastapi import FastAPI
+from fastapi import Request, FastAPI
 from pydantic import BaseModel
+from utilities.persistence import Persist
 import json
 import structlog
+from structlog.contextvars import (
+    bind_contextvars,
+    bound_contextvars,
+    clear_contextvars,
+    merge_contextvars,
+    unbind_contextvars,
+)
 
+from structlog import configure
 
 app = FastAPI()
 
-
-class InvokeRequestData(BaseModel):
-	appID: str
-	highBP: int
-	highChol: int
-	cholCheck: int
-	bmi: int
-	smoker: int
-	stroke: int
-	heartDiseaseOrAttack: int
-	physicalActivity: int
-	fruits: int
-	veggies: int
-	heavyAlcoholConsumption: int
-	anyHealthCare: int
-	noDocBecauseOfCost: int
-	generalHealthSelfAssessment: int
-	mentalHealthIssues: int
-	physicalHealthIssues: int
-	difficultyWalking: int
-	sex: int
-	age: int
-	education: int
-	income: int
+configure(processors=[merge_contextvars, structlog.processors.JSONRenderer()])
+logger = structlog.get_logger()
 
 
 @app.post("/invoke")
-async def invoke(data: InvokeRequestData):
-    # TODO: persist data in duckdb
-    
+async def invoke(request: Request):
+    try:
+    	# clear the context variables for each request
+    	clear_contextvars()
+    	# get trace id and json data from request
+    	trace = request.headers.get("trace")
+    	data = await request.json()
 
+        # persist data in lake
+    	bind_contextvars(traceID=trace)
+    	logger.info("Persisting data...")
+    	Persist.push(data, local=True)
+    	# logger.info(f"data for {data['appID']} persisted")
+    except Exception as exc:
+    	logger.error("Persistence failed", exc_info=True)
+
+    	return {"error": "fix"}
     # TODO: send data to model service
-
+    return await request.json()
 
 @app.get("/ping")
 async def check():
-	return "Ingestion Service is healthy!"
+	return "Ingestion Service is up!"
