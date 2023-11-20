63614a1f9f259e046311891ff32bb110a6eb307e
nsgorlew <nsgorlew@gmail.com>
2023-11-19 22:31:09 -0500

    added data service endpoint

--- src/ingestion-service/src/main.py
+++ src/ingestion-service/src/main.py
@@ -1,9 +1,10 @@
-from fastapi import Request, FastAPI
-from pydantic import BaseModel
+from fastapi import Request, FastAPI, Response, HTTPException
+# from pydantic import BaseModel, Field, ValidationError
 from utilities.persistence import Persist
 from utilities.request_handling import RequestHandler
 import json
-import jsonschema
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
 import structlog
 from structlog.contextvars import (
     bind_contextvars,
@@ -20,8 +21,33 @@ app = FastAPI()
 configure(processors=[merge_contextvars, structlog.processors.JSONRenderer()])
 logger = structlog.get_logger()
 
+"""
+class RequestSchema(BaseModel):
+    appID: int = Field(gt=0)
+    highBP: bool
+    highChol: bool
+    bmi: int = Field(ge=10, lt=80)
+    smoker: bool
+    stroke: bool
+    heartDiseaseOrAttack: bool
+    physicalActivity: bool
+    fruits: bool
+    veggies: bool
+    heavyAlcoholConsumption: bool
+    anyHealthCare: bool
+    noDocBecauseOfCost: bool
+    generalHealthSelfAssessment: int = Field(ge=0, lt=6)
+    mentalHealthIssues: int = Field(ge=0, le=30)
+    physicalHealthIssues: int = Field(ge=0, le=30)
+    difficultyWalking: bool
+    sex: bool
+    age: int = Field(ge=0, lt=130)
+    education: int = Field(ge=1, le=6)
+    income: int = Field(ge=1, le=8)
+"""
 
-@app.post("/invoke")
+
+@app.post("/ingestion/invoke")
 async def invoke(request: Request):
     try:
         # clear the context variables for each request
@@ -30,11 +56,12 @@ async def invoke(request: Request):
         trace = request.headers.get("trace")
         data = await request.json()
 
-        # validate data against schema
+        # validate the data against basemodel
         try:
-            schema = json.load("utilities/validation/validator-schema.json")
-            jsonschema.validate(instance=data, schema=schema)
-        except jsonschema.exceptions.ValidationError as ve:
+            schema = json.load(open("C:/GitHub/mlops-playground/src/ingestion-service/src/utilities/validation/validator-schema.json"))
+            validate(instance=data, schema=schema)
+        except ValidationError as ve:
+            logger.error(f"{str(ve)}")
             raise ve
 
         # persist data in lake
@@ -50,7 +77,7 @@ async def invoke(request: Request):
     except Exception as exc:
         logger.error("Persistence failed", exc_info=True)
 
-        return {"error": "fix"}
+        return {"error": str(exc)}
     # TODO: send data to model service
     return await request.json()
 
