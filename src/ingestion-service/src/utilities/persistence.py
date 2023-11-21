bf543b89e1a49e1cee0fea46ad1379cacf21547d
nsgorlew <nsgorlew@gmail.com>
2023-11-21 08:16:29 -0500

    added boto3 persistence with minio

--- src/ingestion-service/src/utilities/persistence.py
+++ src/ingestion-service/src/utilities/persistence.py
@@ -1,25 +1,38 @@
 from datetime import datetime, date
+
+import boto3
 import json
 import os
 
+
 class Persist:
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	@staticmethod
-	def push(data, local=True):
-		persistence_path = f"{os.getcwd()}/{date.today()}"
-		if local:
-			if os.path.exists(persistence_path):
-				with open(f"{persistence_path}/{data['appID']}_{datetime.now().strftime('%Y-%m-%d')}.json", "w") as f:
-					json.dump(data, f)
-			else:
-				os.mkdir(persistence_path, 0o666)
-				with open(f"{persistence_path}/{data['appID']}_{datetime.now().strftime('%Y-%m-%d')}.json", "w") as f:
-					json.dump(data, f)
-					
-			return True
-		else:
-			# TODO: push to cloud data lake
-			return True
+    @staticmethod
+    def push(data, local=False):
+        """
+        persistence_path = f"{os.getcwd()}/{date.today()}"
+        if local:
+            if os.path.exists(persistence_path):
+                with open(f"{persistence_path}/{data['appID']}_{datetime.now().strftime('%Y-%m-%d')}.json", "w") as f:
+                    json.dump(data, f)
+            else:
+                os.mkdir(persistence_path, 0o666)
+                with open(f"{persistence_path}/{data['appID']}_{datetime.now().strftime('%Y-%m-%d')}.json", "w") as f:
+                    json.dump(data, f)
+
+            return True
+        """
+        bucket = "mlops-playground"
+        key = f"/data/{data['appID']}_{datetime.now().strftime('%Y-%m-%d')}.json"
+
+        # using credentials for minio local server
+        s3 = boto3.client("s3",
+                          endpoint_url="http://localhost:9000",
+                          aws_access_key_id="fPa43Tw12ozMPvHo78QO",
+                          aws_secret_access_key="q95TYfQvZH1PUFYaPftk5nbnP3TWl59MtXgyY6K8"
+                          )
+        s3.put_object(Body=json.dumps(data), Bucket=bucket, Key=key)
+        return True
